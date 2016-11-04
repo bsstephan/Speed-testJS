72ea7f27b5b536c572e902c4cc93cdb532c6f1a0
Phil Courtier <phicou+github@gmail.comhil>
2016-11-04 08:09:19 -0500

    Fix some quirks in serverPing
    Remove need for ip query parameter
    remove unused code
    fix packages.json mistake

--- modules/serverPing.js
+++ modules/serverPing.js
@@ -10,7 +10,8 @@ var netping = require ("net-ping");
 var ping = require ("ping");
 
 function pingIp(nodereq, noderes, ip, protocol) {
-    console.log("ping start");
+    // note - only ipv4 is supported for now
+    console.log("ping start to " + ip + " on " + protocol);
     var error = null;
     try {
         ping.promise.probe(ip).then(
@@ -28,66 +29,7 @@ function pingIp(nodereq, noderes, ip, protocol) {
 }
 
 
-/********************************************************************************************************************/
-
-// // netping methods not currently working
-// function netpingIp(req, res, ip, protocol) {
-//     console.log("netping start");
-//     var protocolVersion = null;
-//     var error = null;
-//     try {
-//         if (protocol == '4') {
-//             protocolVersion = netping.NetworkProtocol.IPv4;
-//         } else if (protocol == '6') {
-//             protocolVersion = netping.NetworkProtocol.Ipv6;
-//         } else {
-//             errorMessage = "Invalid protocol version " + protocol;
-//             console.log(errorMessage);
-//             throw errorMessage;
-//         }
-//         console.log("netping protocol determined");
-//         var session = netping.createSession( {
-//             networkProtocol: protocolVersion,
-//             packetSize: 16, // ICMP packet size in bytes 12 minimum, 16 default
-//             retries: 1,
-//             sessionId: (process.pid % 65535),
-//             timeout: 1000,
-//             ttl: 128
-//         });
-//         runNetping(req, res, responses, errors, desired_response_count, session, target);
-//         console.log("netping running");
-//     }
-//     catch (error) {
-//         console.log(error);
-//     }
-//     return {'latencies': [1, 1, 2, 3, 5, 8, 13, 21, 34, 55], 'protocolVersion': protocolVersion, 'error': error};
-// }
-//
-// function processNetping(req, res, responses, errors, desired_response_count, session, error, target, sent, rcvd) {
-//     console.log("process ping " + sent + " - " + rcvd);
-//     var duration = rcvd - sent;
-//     if (error != null) {
-//         console.log("Error from " + target + " : " + error);
-//         errors.push(duration);
-//     } else {
-//         responses.push(duration);
-//     }
-//     runNetping(req, res, responses, errors, desired_response_count, session, target);
-//     console.log("pp running ping.  result was " + duration);
-// }
-//
-// function runNetping(req, res, responses, errors, desired_response_count, session, target) {
-//     console.log("runping");
-//     if ((responses.len() + errors.len()) < desired_response_count) {
-//         session.pingHost(target, function(error, target, sent, rcvd) {
-//             processNetping(req, res, responses, desired_response_count, session, error, target, sent, rcvd);
-//         })
-//     } else {
-//         res.send({'responses': responses, 'errors': errors, 'target': target});
-//     }
-// }
 
 module.exports = {
-    pingIp: pingIp,
-    // netpingIp: netpingIp
+    pingIp: pingIp
 };
